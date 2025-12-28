eca90b0cdb68f2b5bdde46177b83f5cbf52a07d6
jacopo.congia <jacopo.congia@gmail.com>
2025-12-28 17:05:25 +0100

    riscritto il codice quasi completamente

--- imitazione_vector_container/Vettore.h
+++ imitazione_vector_container/Vettore.h
@@ -1,16 +1,87 @@
-#pragma once
+#include <iostream>
 
+template<typename T>
 class Vettore {
 private:
-	int* p;
-	int* temp;	// puntatore di appoggio
-	int dim;	// dimensione del vettore
+	T *p;
+	int dim; // dimensione del vettore
+	int capacita;
+
 public:
 	// costruttori
-	Vettore();	// costruttore per inizializzare il vettore
+	Vettore(); // costruttore per inizializzare il vettore
+	// distruttore
+	~Vettore();
 
 	// metodi
-	void push(int valore);		// metodo per inserire un valore nel vettore
-	void pop();					// metodo per rimuovere l'ultimo valore dal vettore
-	void visualizzaValori();	// metodo per visualizzare i valori del vettore
+	void push_back(T valore); // metodo per inserire un valore nel vettore
+	void pop_back(); // metodo per rimuovere l'ultimo valore dal vettore
+	void visualizzaValori(); // metodo per visualizzare i valori del vettore
+	int getDim(); // restituisce la dimensione corrente del vettore
+	int getCapacita(); // restituisce la capacita' corrente del vettore
 };
+
+// costruttore di default della classe Vettore
+template<typename T>
+Vettore<T>::Vettore() {
+	p = nullptr; // inizializza il puntatore p a nullptr, versione moderna di NULL
+	dim = 0; // inizializza la dimensione del vettore a 0
+	capacita = 0; // inizializza la capacita' del vettore 0
+}
+
+// distruttore
+template<typename T>
+Vettore<T>::~Vettore() {
+	delete[] p;
+	std::cout << "Sono il distruttore: Vettore eliminato" << std::endl;
+}
+
+// metodo push_back (inserisce un valore nel vettore)
+template<typename T>
+void Vettore<T>::push_back(T valore) {
+	if (dim == capacita) {
+		// operatore ternario, se la capacita' (e la dimensione) sono a 0, la imposto a 1, altrimenti la raddoppio (devo fare cosi' altrimenti raddoppierei 0 in caso di capacita'/dim = 0)
+		capacita = (capacita == 0) ? 1 : capacita * 2;
+
+		// alloca spazio per un array di dimensione "capacita" e assegna l'indirizzo inziale al puntatore "temp"
+		T *temp = new T[capacita];
+		for (int i = 0; i < dim; i++) {
+			*(temp + i) = *(p + i);
+		}
+		delete[] p; // libera la memoria occupata da "p" che ora non serve piu' perche' puntera' a temp
+		p = temp; // assegno il puntatore temp al puntatore p
+	}
+	// a questo punto lo spazio c'e' sicuramente, quindi basta assegnare il valore a p in posizione dim (ho usato sempre l'aritmetica dei puntatori per fare pratica)
+	*(p + dim) = valore;
+	dim++; // incremento dim
+}
+
+// metodo pop_back (rimuove l'ultimo elemento del vettore)
+template<typename T>
+void Vettore<T>::pop_back() {
+	if (dim > 0) {
+		// controllo che il vettore non sia vuoto
+		dim--; // diminuisco la dimensione del vettore
+	}
+}
+
+// visualizza i valori contenuti nel vettore
+template<typename T>
+void Vettore<T>::visualizzaValori() {
+	for (int i = 0; i < dim; i++) {
+		std::cout << *(p + i) << " ";
+	}
+	std::cout << std::endl;
+}
+
+// restituisce la dimensione corrente del vettore
+template<typename T>
+int Vettore<T>::getDim() {
+	return dim;
+}
+
+// restituisce la capacita' corrente del vettore
+template<typename T>
+int Vettore<T>::getCapacita() {
+	return capacita;
+}
--- imitazione_vector_container/imitazione_vector_container.cpp
+++ imitazione_vector_container/imitazione_vector_container.cpp
@@ -1,69 +1,31 @@
 /******************************************************************************
-Implementazione di push e pop mantenendo la contiguita' dell'array
-creare / distruggere vettori
+Imitazione della classe template Vector del C++
 
-separare push e pop
+Obiettivi:
+X   -Trasformare in classe template
+X   -Implementare la capacita' del vettore
+    -Altri Costruttori
+X   -Distruttore
+    -Iteratori
 *******************************************************************************/
 #include <iostream>
+#include <ctime> // versione c della libreria time
+#include <cstdlib> // versione c della libreria stdlib (per srand e rand)
 #include "Vettore.h"
 
-int main()
-{
-    //// partiamo dalla funzione push
-    //int* p = NULL;
-    //int* temp = NULL; // puntatore di appoggio
-    //int dim = 0; // dimensione del vettore
-    //int valore; // valore inserito dall'utente ad ogni ciclo
-    //int scelta = 0; // scelta dell'utente (continua o no) che verra' usata come condizione di uscita dal ciclo
+int main() {
+    srand(time(NULL));
 
-    //while (scelta != 2) {
-    //    dim++; // viene incrementato il valore della variabile "dim" (dimensione dell'array) di 1
-    //    std::cout << "Inserisci un valore: " << std::endl;
-    //    std::cin >> valore; // viene assegnato il valore scelto dall'utente alla variabile "valore"
-    //    // codice da eseguire solo al primo ciclo
-    //    if (dim <= 1) {
-    //        p = new int[dim];
-    //        p[0] = valore;
-    //        /* codice da eseguire solo se il valore della variabile "dim" e' maggiore di 1
-    //        (cioe' se l'utente sceglie di aggiungere un altro valore) */
-    //    }
-    //    else {
-    //        // std::cout<<"Temp: "<<temp<<std::endl; // prova per controllare il valore di temp prima di usare "new"
-    //        temp = new int[dim - 1]; /* alloca spazio per un array di dimensione "dim - 1" e assegna
-    //                                    l'indirizzo inziale al puntatore "temp", ho scelto dim - 1 come dimensione
-    //                                    perche' l'array di appoggio "temp" ha sempre bisogno di un elemento in meno
-    //                                    rispetto a "p", visto che l'ultimo valore viene aggiunto a "p" direttamente ad ogni ciclo */
-    //                                    // std::cout<<"Temp: "<<temp<<std::endl; // prova per controllare il valore di temp dopo aver usato "new"
-    //                                    // ciclo che copia i valori del vettore "p" nel vettore di appoggio "temp"
-    //        for (int i = 0; i < dim - 1; i++) {
-    //            *(temp + i) = *(p + i);
-    //        }
-    //        delete[]p; // libera la memoria occupata da "p" che ora non serve piu' perche' verra' ricreato con un elemento in piu'
-    //        p = new int[dim]; // viene allocato spazio per un array di interi di dimensione "dim" e l'indirizzo del primo elemento viene assegnato a "p"
-    //        /* ciclo che copia i valori dal vettore di appoggio "temp" al vettore "p", il ciclo si ferma quando l'indice e' minore di dim - 1
-    //           perche' l'ultimo elemento verra' riempito dal valore inserito dall'utente (contenuto nella variabile "valore") */
-    //        for (int i = 0; i < dim - 1; i++) {
-    //            *(p + i) = *(temp + i);
-    //        }
-    //        p[dim - 1] = valore; // assegnazione del valore inserito dall'utente (contenuto nella variabile "valore") nell'ultimo elemento dell'array "p"
-    //        delete[]temp;  // libera la memoria occupata dall'array "temp", che verra' ricreato al prossimo ciclo con un elemento in piu'
-    //    }
-    //    // std::cout<<"Numero inserito: "<<*p<<std::endl;
-    //    // std::cout<<"Dimensione array: "<<dim<<std::endl;
-    //    std::cout << "Valori nell'array: " << std::endl;
-    //    for (int i = 0; i < dim; i++) {
-    //        std::cout << *(p + i) << " ";
-    //    }
-    //    std::cout << std::endl << std::endl;
-    //    std::cout << "Vuoi inserire un altro valore?" << std::endl;
-    //    std::cout << "1. Si" << std::endl << "2. No" << std::endl;
-    //    std::cin >> scelta;
-    //};
-
-    Vettore test;
-    test.push(5);
-    test.push(12);
+    Vettore<int> test;
+    std::cout << "Dimensione iniziale: " << test.getDim() << std::endl;
+    std::cout << "Capacita' iniziale: " << test.getCapacita() << std::endl;
+    std::cout << "I valori nel vettore sono: " << std::endl;
+    for (int i = 0; i < 10; i++) {
+        test.push_back(rand() % 100 + 1);
+    }
     test.visualizzaValori();
+    std::cout << "Dimensione finale: " << test.getDim() << std::endl;
+    std::cout << "Capacita' finale: " << test.getCapacita() << std::endl;
 
     return 0;
 }
